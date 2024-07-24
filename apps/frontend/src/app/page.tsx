1428df1a9477fbbbc21e892cd18f60494e2b3ccb
Breno Marcomini <brenomarcomini@hotmail.com>
2024-07-23 22:14:43 -0300

    core

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,8 +1,7 @@
 import Image from "next/image";
-import {somar} from "@barba/core"
 
 export default function Home() {
   return (
-    <div>{somar(20,30)}</div>
+    <div>FrontEnd</div>
   );
 }
---
+++ packages/core/src/profissional/Profissional.ts
@@ -0,0 +1,8 @@
+export default interface Profissional {
+    id: number
+    nome: string
+    descricao: string
+    imageUrl: string
+    avaliacao: number
+    quantidadeAvaliacoes: number
+}
